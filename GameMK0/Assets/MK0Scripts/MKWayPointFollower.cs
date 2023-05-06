cba60be538195c9d86dbe6cc6f4768fd09391cf4
Fede9308 <117412714+Fede9308@users.noreply.github.com>
2023-05-06 14:19:00 -0300

    PlatformSpace1
    The first two moving platforms were added

---
+++ GameMK0/Assets/MK0Scripts/MKWayPointFollower.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MKWayPointFollower : MonoBehaviour
+{
+    [SerializeField] private GameObject[] wayPoints;
+    private int currentWayPointIndex = 0;
+
+    [SerializeField] private float speed = 2.0f;
+    
+    private void Update()
+    {
+        if (Vector2.Distance(wayPoints[currentWayPointIndex].transform.position, transform.position) < 0.1f)
+        {
+            currentWayPointIndex++;
+            if (currentWayPointIndex >= wayPoints.Length)
+            {
+                currentWayPointIndex = 0;                 
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, wayPoints[currentWayPointIndex].transform.position, Time.deltaTime * speed);
+
+    }
+}
