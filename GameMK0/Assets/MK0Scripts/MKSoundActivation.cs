314c94fe017afd989daae15d699abc03c22be567
Fede9308 <117412714+Fede9308@users.noreply.github.com>
2023-05-09 02:44:59 -0300

    Finished Pc build
    This is the pc build

--- GameMK0/Assets/MK0Scripts/MKSoundActivation.cs
+++ GameMK0/Assets/MK0Scripts/MKSoundActivation.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 
 public class MKSoundActivation : MonoBehaviour
 {
-   [SerializeField]  GameObject audioSourceObject;  // Drag and drop the audio source object in the inspector
+   [SerializeField]  GameObject audioSourceObject;  
     private AudioSource audioSource;
     private bool isAudioOn;
 
     private void Start()
     {
         audioSource = audioSourceObject.GetComponent<AudioSource>();
-        isAudioOn = PlayerPrefs.GetInt("IsAudioOn", 1) == 1;  // Load the audio state from player prefs
+        isAudioOn = PlayerPrefs.GetInt("IsAudioOn", 1) == 1;  
         audioSource.gameObject.SetActive(isAudioOn);
     }
 
@@ -20,6 +21,7 @@ public class MKSoundActivation : MonoBehaviour
     {
         isAudioOn = !isAudioOn;
         audioSource.gameObject.SetActive(isAudioOn);
-        PlayerPrefs.SetInt("IsAudioOn", isAudioOn ? 1 : 0);  // Save the audio state to player prefs
+        PlayerPrefs.SetInt("IsAudioOn", isAudioOn ? 1 : 0);
+        EventSystem.current.SetSelectedGameObject(null);
     }
 }
